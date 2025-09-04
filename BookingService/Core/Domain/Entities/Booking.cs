c36fcd86bf37222e1597b8538b0a3cc5edbb70fd
Samuel Santos <samuel.santos@inventsoftware.com.br>
2025-09-04 13:12:09 -0300

    created entities and basics enums #time 10m

---
+++ BookingService/Core/Domain/Entities/Booking.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Enums;
+
+namespace Domain.Entities
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public DateTime PlaceAt { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        private StatusBooking Status { get; set; }
+        public StatusBooking CurrentStatus { get { return this.Status; } }
+    }
+}
---
+++ BookingService/Core/Domain/Entities/Room.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public int Level { get; set; }
+        public bool IsAvaliable { get; set; }
+    }
+}
