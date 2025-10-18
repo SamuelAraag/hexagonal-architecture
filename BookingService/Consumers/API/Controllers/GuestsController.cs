b08ab968ef48adbbf568a673106fd09f40383a18
Samuel Santos <samuel.santos@inventsoftware.com.br>
2025-10-18 12:07:04 -0300

    create guestscontroller

---
+++ BookingService/Consumers/API/Controllers/GuestsController.cs
@@ -0,0 +1,30 @@
+﻿using Application.Guests.DTOs;
+using Application.Guests.Ports;
+using Application.Guests.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GuestsController : ControllerBase
+    {
+        private readonly ILogger<GuestsController> _logger;
+        private readonly IGuestManager _guestManager;
+
+        public GuestsController(ILogger<GuestsController> logger, IGuestManager guestManager)
+        {
+            _logger = logger;
+            _guestManager = guestManager;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<GuestDTO>> Post(CreateGuestRequest guest)
+        {
+            var res = await _guestManager.Create(guest);
+
+            if(res.su)
+
+        }
+    }
+}
